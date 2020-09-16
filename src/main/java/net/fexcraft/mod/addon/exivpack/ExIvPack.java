20dfecb6a1751cfe0de145bc56e24937b70fcef5
Ferdinand <fercalo96@yahoo.de>
2020-09-16 21:55:10 +0200

    Update ExIvPack.java

--- src/main/java/net/fexcraft/mod/addon/exivpack/ExIvPack.java
+++ src/main/java/net/fexcraft/mod/addon/exivpack/ExIvPack.java
@@ -4,47 +4,54 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Method;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
-import minecrafttransportsimulator.dataclasses.MTSRegistry;
-import minecrafttransportsimulator.systems.PackParserSystem;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.discovery.ContainerType;
-import net.minecraftforge.fml.common.discovery.ModCandidate;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = ExIvPack.MODID, name = ExIvPack.NAME, version = ExIvPack.VERSION, useMetadata = true, dependencies = "required-after:mts@[18.0.0,);")
 public class ExIvPack {
-	
-    public static final String MODID = "exivpack";
-    public static final String NAME = "Experimental Pack";
-    public static final String VERSION = "1.0";
 
-    /** code to get file partially from fvtm */
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event){
-		ModCandidate mod = (ModCandidate)event.getAsmData().getCandidatesFor("net.fexcraft.mod.addon.exivpack").toArray()[0];
-		ContainerType type = mod.getSourceType();
-		System.out.println(mod.getModContainer());
-    	for(String str : PackParserSystem.getValidPackContentNames()){
+	public static final String MODID = "exivpack";
+	public static final String NAME = "Experimental Pack";
+	public static final String VERSION = "1.0";
+
+	public ExIvPack(){
+		try{
+			String packageName = this.getClass().getPackage().getName();
+			String classDir = null;
 			try{
-	    		ArrayList<String> entryNames = new ArrayList<String>();
-				Method addContentMethod = PackParserSystem.class.getMethod("add" + str.substring(0, 1).toUpperCase() + str.substring(1) + "Definition", InputStreamReader.class, String.class, String.class);
-				if(type == ContainerType.JAR){
-					ZipFile jarFile = new ZipFile(mod.getModContainer());
+				classDir = this.getClass().getClassLoader().getResource(packageName).getPath();
+				URI classURI = new URI(classDir);
+				classDir = classURI.getPath();
+				classDir = classDir.substring(0, classDir.indexOf('!'));
+			}
+			catch(Exception e){
+				classDir = "../src/main/resources/";
+			}
+			Class<?> packParserSystem = Class.forName("minecrafttransportsimulator.systems.PackParserSystem");
+			Method getPackContentNamesMethod = packParserSystem.getMethod("getValidPackContentNames");
+			String[] contentNames = (String[])getPackContentNamesMethod.invoke(null);
+			for(String contentName : contentNames){
+				List<String> entryNames = new ArrayList<String>();
+				Method addContentMethod = packParserSystem.getMethod("add" + contentName.substring(0, 1).toUpperCase() + contentName.substring(1) + "Definition", InputStreamReader.class, String.class, String.class);
+				File file = new File(classDir);
+				System.out.println(file);
+				System.out.println(file.toURI().toURL().toString());
+				if(!file.isDirectory()){
+					ZipFile jarFile = new ZipFile(classDir);
 					Enumeration<? extends ZipEntry> entries = jarFile.entries();
 					while(entries.hasMoreElements()){
 						ZipEntry entry = entries.nextElement();
-						if(entry.getName().endsWith(".json") && entry.getName().contains("jsondefs/" + str + "s")){
+						if(entry.getName().endsWith(".json") && entry.getName().contains("jsondefs/" + contentName + "s")){
 							entryNames.add(entry.getName());
 						}
 					}
@@ -53,26 +60,29 @@ public class ExIvPack {
 						String entryFileName = entryName.substring(entryName.lastIndexOf('/') + 1, entryName.length() - ".json".length());
 						addContentMethod.invoke(null, new InputStreamReader(jarFile.getInputStream(jarFile.getEntry(entryName)), "UTF-8"), entryFileName, MODID);
 					}
+					jarFile.close();
 				}
-				else{//DIR
-					File folder = new File(mod.getModContainer(), "/assets/" + MODID + "/jsondefs/" + str + "s");
+				else{
+					File folder = new File(file, "/assets/" + MODID + "/jsondefs/" + contentName + "s");
 					if(!folder.exists()) folder.mkdirs();
-					for(File file : folder.listFiles()){
-						if(!file.getName().endsWith(".json")) continue;
-						addContentMethod.invoke(null, new InputStreamReader(new FileInputStream(file)), file.getName().replace(".json", ""), MODID);
+					for(File fl : folder.listFiles()){
+						if(!fl.getName().endsWith(".json")) continue;
+						addContentMethod.invoke(null, new InputStreamReader(new FileInputStream(fl)), fl.getName().replace(".json", ""), MODID);
 					}
 				}
 			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
-    	}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
-    
+
 	@SubscribeEvent
 	public static void registerItems(RegistryEvent.Register<Item> event){
 		try{
-			List<Item> itemList = MTSRegistry.getItemsForPack(MODID);
+			Class<?> registry = Class.forName("minecrafttransportsimulator.dataclasses.MTSRegistry");
+			Method getItemsMethod = registry.getMethod("getItemsForPack", String.class);
+			List<Item> itemList = (List<Item>)getItemsMethod.invoke(null, MODID);
 			for(Item item : itemList){
 				item.setRegistryName(new ResourceLocation(MODID, item.getTranslationKey().replace("item." + MODID + ".", "")));
 				event.getRegistry().register(item);
@@ -80,8 +90,7 @@ public class ExIvPack {
 		}
 		catch(Exception e){
 			e.printStackTrace();
-			FMLCommonHandler.instance().exitJava(1, true);
 		}
 	}
-    
+
 }
